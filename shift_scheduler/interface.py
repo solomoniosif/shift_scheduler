1617381ba48a247ef5d1877b8beeaf5f20b56607
iosif <solomoniosif@gmail.com>
2022-06-08 22:03:25 +0300

    rewrote local modules imports to be able to run the app or tests from any location

--- shift_scheduler/interface.py
+++ shift_scheduler/interface.py
@@ -1,4 +1,5 @@
 import calendar
+import secrets
 from datetime import date, datetime
 from functools import cached_property
 from typing import List, Tuple
@@ -7,8 +8,17 @@ import pygsheets
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
-import secrets
-from utils import TimerLog
+try:
+    from shift_scheduler import secrets
+    from shift_scheduler.utils import TimerLog
+except ImportError:
+    import sys
+    from pathlib import Path
+
+    root_folder = Path(__file__).parent.parent.absolute()
+    sys.path.append(str(root_folder))
+    from shift_scheduler import secrets
+    from shift_scheduler.utils import TimerLog
 
 
 class ScheduleSSManager:
@@ -112,7 +122,7 @@ class ScheduleSSManager:
         self.month = month
         self.month_name = ScheduleSSManager.MONTH_NAMES[self.month - 1]
         self.client = pygsheets.authorize(
-            service_account_file="iosif_upu_service_account.json"
+            service_account_file=secrets.PATH_TO_SERVICE_ACCOUNT_FILE
         )
         self.this_month_ss = None
 
@@ -159,7 +169,7 @@ class ScheduleSSManager:
             self.this_month_ss = self.client.open_by_key(new_ss_id)
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def holiday_list(self) -> List[date]:
         ro_holidays_raw = self.settings_ss.worksheet_by_title("Sarbatori legale").range(
             "C3:C212", returnas="matrix"
@@ -284,37 +294,37 @@ class ScheduleSSManager:
         return self.this_month_ss.url
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def nurse_list(self) -> List[list]:
         nurses_ws = self.settings_ss.worksheet_by_title("Asistenti")
         return nurses_ws.range("A3:AB101", returnas="matrix")
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def nurse_min_max(self) -> List[list]:
         nurses_ws = self.settings_ss.worksheet_by_title("Asistenti")
         return nurses_ws.range("AD3:BE102", returnas="matrix")
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def sector_list(self) -> List[list]:
         sectors_ws = self.settings_ss.worksheet_by_title("Sectoare")
         return sectors_ws.range("A3:F20", returnas="matrix")
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def position_list(self) -> List[list]:
         sectors_ws = self.settings_ss.worksheet_by_title("Sectoare")
         return sectors_ws.range("N3:P22", returnas="matrix")
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def sectors_priority(self) -> List[list]:
         sectors_ws = self.settings_ss.worksheet_by_title("Sectoare")
         return sectors_ws.range("N3:O22", returnas="matrix")
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def planned_rest_leaves(self) -> List[list]:
         rl_ws = self.settings_ss.worksheet_by_title("CO")
         return rl_ws.get_values(
@@ -334,12 +344,12 @@ class ScheduleSSManager:
         return calendar.monthrange(self.year, self.month)[1]
 
     @cached_property
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def fixed_assignments(self) -> List[list]:
         planificator = self.this_month_ss.worksheet_by_title("Planificator")
         return planificator.range("A16:BM115", returnas="matrix")
 
-    @TimerLog(logger_name='scheduler.interface')
+    @TimerLog(logger_name='shift_scheduler.interface')
     def insert_shifts(self, solution_matrix) -> None:
         planificator = self.this_month_ss.worksheet_by_title("Planificator")
         planificator.update_values(crange="D16", values=solution_matrix)
