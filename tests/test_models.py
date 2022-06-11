6c28576e746e851d5b9182961a03aa3c878a2f38
iosif <solomoniosif@gmail.com>
2022-06-11 21:39:13 +0300

    added more fixtures & tests for Sector, Position and Nurse classes

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@ from datetime import date
 import pytest
 
 try:
-    from shift_scheduler import models
+    from shift_scheduler import interface, models
 except ImportError:
     import sys
     from pathlib import Path
@@ -11,14 +11,19 @@ except ImportError:
     root_folder = Path(__file__).parent.parent.absolute()
     sys.path.append(str(root_folder))
 
-    from shift_scheduler import models
+    from shift_scheduler import interface, models
+
+
+@pytest.fixture(scope="session")
+def ss_manager():
+    return interface.ScheduleSSManager(2022, 5)
 
 
 #################################
 #   Tests for TimeSlot Class    #
 #################################
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def sample_timeslot():
     return models.TimeSlot(date(2022, 1, 1), 1)
 
@@ -67,12 +72,12 @@ def test_timeslot_is_adjacent_ts_function(sample_timeslot):
 #################################
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def january_legal_holidays():
     return [date(2022, 1, 1), date(2022, 1, 2), date(2022, 1, 24)]
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def sample_month(january_legal_holidays):
     return models.Month(2022, 1, january_legal_holidays)
 
@@ -146,8 +151,31 @@ def test_month_timeslots_per_cycle(sample_month):
 #################################
 
 
-@pytest.fixture()
-def all_sectors():
+@pytest.fixture(scope="session")
+def all_sectors(ss_manager):
+    sectors = []
+    for row in ss_manager.sector_list:
+        if row[1] != "":
+            sectors.append(
+                models.Sector(
+                    id=int(row[0]),
+                    short_name=row[2],
+                    full_name=row[1],
+                    min_nurses=int(row[3]),
+                    optimal_nurses=int(row[4]),
+                    max_nurses=int(row[5]),
+                )
+            )
+    return sectors
+
+
+@pytest.fixture(scope="session")
+def sectors_lookup(all_sectors):
+    return {s.short_name: s for s in all_sectors}
+
+
+@pytest.fixture(scope="session")
+def sample_sectors():
     sectors_data = [
         [1, 'Responsabil tura', 'Rt', 1, 1, 1],
         [2, 'Triaj', 'T', 1, 1, 2],
@@ -177,7 +205,7 @@ def test_all_sectors(all_sectors):
     assert len({s.id for s in all_sectors}) == 15
 
 
-def test_all_sectors_min_lte_max_nurses(all_sectors):
+def test_sector_min_lte_max_nurses(all_sectors):
     for sector in all_sectors:
         assert sector.min_nurses <= sector.max_nurses
 
@@ -193,3 +221,71 @@ def test_sector_min_max_optimal_is_int(all_sectors):
         assert type(sector.min_nurses) == int
         assert type(sector.optimal_nurses) == int
         assert type(sector.max_nurses) == int
+
+
+#################################
+#   Tests for Position Class    #
+#################################
+
+
+@pytest.fixture(scope="session")
+def sample_position():
+    return models.Position(1, 'Rt', models.Sector(1, 'Rt', 'Responsabil tura', 1, 1, 1))
+
+
+@pytest.fixture(scope="session")
+def all_positions(ss_manager, sectors_lookup):
+    positions = []
+    for row in ss_manager.position_list:
+        if row[2] != "":
+            id = int(row[0])
+            name = row[2]
+            sector = sectors_lookup[row[1]]
+            positions.append(models.Position(id, name, sector))
+    return positions
+
+
+def test_position_sector_type(sample_position, all_positions):
+    assert type(sample_position.sector) == models.Sector
+    for position in all_positions:
+        assert type(position.sector) == models.Sector
+
+
+def test_different_position_ids(all_positions):
+    assert len(all_positions) == len({p.id for p in all_positions})
+
+
+def test_position_eq(all_positions, sectors_lookup):
+    assert all_positions[0] != all_positions[1]
+    assert all_positions[0] == models.Position(1, 'Rt', sectors_lookup['Rt'])
+
+
+#################################
+#   Tests for Nurse Class       #
+#################################
+
+
+@pytest.fixture(scope="session")
+def all_nurses(ss_manager):
+    nurses = []
+    for row in ss_manager.nurse_list:
+        if row[1] != "":
+            nurse_id = int(row[0])
+            full_name = row[1]
+            first_name = full_name.split(" ")[0]
+            last_name = full_name.split(" ")[-1]
+            shift_cycle = int(row[4]) if row[4].isnumeric() else row[4]
+            sectors_raw = row[5:]
+            sectors = [s for s in sectors_raw if s != ""]
+            extra_hours_worked = int(row[3])
+            nurses.append(
+                models.Nurse(
+                    id=nurse_id,
+                    first_name=first_name,
+                    last_name=last_name,
+                    positions=sectors,
+                    cycle=shift_cycle,
+                    extra_hours_worked=extra_hours_worked,
+                )
+            )
+    return nurses
