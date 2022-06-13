1ada6a9cf05b39246a3518b5e2b634596945c1e0
iosif <solomoniosif@gmail.com>
2022-06-13 15:47:32 +0300

    moved all fixtures to conftest.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,18 +14,9 @@ except ImportError:
     from shift_scheduler import interface, models
 
 
-@pytest.fixture(scope="session")
-def ss_manager():
-    return interface.ScheduleSSManager(2022, 5)
-
-
 #################################
 #   Tests for TimeSlot Class    #
 #################################
-
-@pytest.fixture(scope="session")
-def sample_timeslot():
-    return models.TimeSlot(date(2022, 1, 1), 1)
 
 
 def test_timeslot_date(sample_timeslot):
@@ -70,16 +61,6 @@ def test_timeslot_is_adjacent_ts_function(sample_timeslot):
 #################################
 #   Tests for Month Class       #
 #################################
-
-
-@pytest.fixture(scope="session")
-def january_legal_holidays():
-    return [date(2022, 1, 1), date(2022, 1, 2), date(2022, 1, 24)]
-
-
-@pytest.fixture(scope="session")
-def sample_month(january_legal_holidays):
-    return models.Month(2022, 1, january_legal_holidays)
 
 
 def test_month_date(sample_month):
@@ -151,55 +132,6 @@ def test_month_timeslots_per_cycle(sample_month):
 #################################
 
 
-@pytest.fixture(scope="session")
-def all_sectors(ss_manager):
-    sectors = []
-    for row in ss_manager.sector_list:
-        if row[1] != "":
-            sectors.append(
-                models.Sector(
-                    id=int(row[0]),
-                    short_name=row[2],
-                    full_name=row[1],
-                    min_nurses=int(row[3]),
-                    optimal_nurses=int(row[4]),
-                    max_nurses=int(row[5]),
-                )
-            )
-    return sectors
-
-
-@pytest.fixture(scope="session")
-def sectors_lookup(all_sectors):
-    return {s.short_name: s for s in all_sectors}
-
-
-@pytest.fixture(scope="session")
-def sample_sectors():
-    sectors_data = [
-        [1, 'Responsabil tura', 'Rt', 1, 1, 1],
-        [2, 'Triaj', 'T', 1, 1, 2],
-        [3, 'Resuscitare', 'RCP', 1, 1, 2],
-        [4, 'Urgente majore', 'A', 2, 2, 4],
-        [5, 'Urgente minore', 'M', 2, 2, 4],
-        [6, 'Urgente minore Etaj', 'Me', 1, 1, 2],
-        [7, 'Chirurgie', 'Ch', 0, 1, 1],
-        [8, 'Laborator', 'L', 1, 1, 1],
-        [9, 'Triaj epidemiologic 1', 'C1', 1, 1, 1],
-        [10, 'Triaj epidemiologic 2', 'C2', 0, 1, 2],
-        [11, 'Transport neonatal', 'Nn', 1, 1, 2],
-        [12, 'SMURD', 'S', 1, 1, 2],
-        [13, 'Elicopter Jibou', 'E', 0, 1, 1],
-        [14, '8', '8', 0, 1, 2],
-        [15, '12', '12', 0, 0, 3]
-
-    ]
-    all_sectors = []
-    for row in sectors_data:
-        all_sectors.append(models.Sector(row[0], row[2], row[1], row[3], row[4], row[5]))
-    return all_sectors
-
-
 def test_all_sectors(all_sectors):
     assert all((True if type(s) == models.Sector else False for s in all_sectors))
     assert len({s.id for s in all_sectors}) == 15
@@ -228,23 +160,6 @@ def test_sector_min_max_optimal_is_int(all_sectors):
 #################################
 
 
-@pytest.fixture(scope="session")
-def sample_position():
-    return models.Position(1, 'Rt', models.Sector(1, 'Rt', 'Responsabil tura', 1, 1, 1))
-
-
-@pytest.fixture(scope="session")
-def all_positions(ss_manager, sectors_lookup):
-    positions = []
-    for row in ss_manager.position_list:
-        if row[2] != "":
-            id = int(row[0])
-            name = row[2]
-            sector = sectors_lookup[row[1]]
-            positions.append(models.Position(id, name, sector))
-    return positions
-
-
 def test_position_sector_type(sample_position, all_positions):
     assert type(sample_position.sector) == models.Sector
     for position in all_positions:
@@ -265,27 +180,5 @@ def test_position_eq(all_positions, sectors_lookup):
 #################################
 
 
-@pytest.fixture(scope="session")
-def all_nurses(ss_manager):
-    nurses = []
-    for row in ss_manager.nurse_list:
-        if row[1] != "":
-            nurse_id = int(row[0])
-            full_name = row[1]
-            first_name = full_name.split(" ")[0]
-            last_name = full_name.split(" ")[-1]
-            shift_cycle = int(row[4]) if row[4].isnumeric() else row[4]
-            sectors_raw = row[5:]
-            sectors = [s for s in sectors_raw if s != ""]
-            extra_hours_worked = int(row[3])
-            nurses.append(
-                models.Nurse(
-                    id=nurse_id,
-                    first_name=first_name,
-                    last_name=last_name,
-                    positions=sectors,
-                    cycle=shift_cycle,
-                    extra_hours_worked=extra_hours_worked,
-                )
-            )
-    return nurses
+def test_unique_nurse_id(all_nurses):
+    assert len(all_nurses) == len({nurse.id for nurse in all_nurses})
