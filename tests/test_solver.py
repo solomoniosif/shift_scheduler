5564d7263ca6afbde76aa2a31280bbb372e3e0d7
iosif <solomoniosif@gmail.com>
2022-06-22 22:28:37 +0300

    added 3 tests for model variables

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,3 +1,4 @@
+from ortools.sat.python import cp_model
 import pytest
 
 try:
@@ -9,6 +10,25 @@ except ImportError:
     root_folder = Path(__file__).parent.parent.absolute()
     sys.path.append(str(root_folder))
     from shift_scheduler.solver import ScheduleModel, SolutionCollector
+
+
+def test_model_variables_created(model):
+    assert model.variables
+
+
+def test_model_variables_types(model):
+    assert type(model.variables) == dict
+    for var in model.variables:
+        assert type(model.variables[var]) == cp_model.IntVar
+
+
+def test_model_variables_valid_nurse_positions(schedule, model):
+    for n_id, t_id, s_id in model.variables:
+        nurse = schedule.nurses_lookup[n_id]
+        position_id = int(str(s_id)[-2:])
+        position = schedule.positions_lookup[position_id]
+
+        assert position.sector.short_name in nurse.positions
 
 
 @pytest.mark.xfail
