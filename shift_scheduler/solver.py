e85f81ce6b4e6fc5b417eca4821970caa76abcff
iosif <solomoniosif@gmail.com>
2022-07-13 20:21:01 +0300

    added type hints for all methods in module

--- shift_scheduler/solver.py
+++ shift_scheduler/solver.py
@@ -3,6 +3,7 @@ from ortools.sat.python import cp_model
 
 try:
     from shift_scheduler.utils import TimerLog
+    from shift_scheduler import models
 except ImportError:
     import sys
     from pathlib import Path
@@ -11,6 +12,7 @@ except ImportError:
     sys.path.append(str(root_folder))
 
     from shift_scheduler.utils import TimerLog
+    from shift_scheduler import models
 
 
 class ScheduleModel(cp_model.CpModel):
@@ -24,13 +26,13 @@ class ScheduleModel(cp_model.CpModel):
     with the given constraints.
     """
 
-    def __init__(self, schedule):
+    def __init__(self, schedule: models.Schedule):
         super().__init__()
         self.schedule = schedule
         self.variables = self._create_shift_variables()
         self._add_constraints()
 
-    def _create_shift_variables(self):
+    def _create_shift_variables(self) -> dict[tuple[int, int, int], cp_model.IntVar]:
         variables = {}
         for nurse in self.schedule.available_nurses:
             for timeslot in self.schedule.month.timeslots:
@@ -54,11 +56,11 @@ class ScheduleModel(cp_model.CpModel):
 
         return variables
 
-    def _add_fixed_assignment(self):
+    def _add_fixed_assignment(self) -> None:
         for shift in self.schedule.fixed_assignments:
             self.Add(self.variables[(shift.assigned_nurse.id, shift.timeslot.id, shift.id)] == 1)
 
-    def _add_no_double_assignment(self):
+    def _add_no_double_assignment(self) -> None:
         for timeslot in self.schedule.month.timeslots:
             for shift in self.schedule.all_shifts[str(timeslot)]:
                 self.Add(
@@ -69,7 +71,7 @@ class ScheduleModel(cp_model.CpModel):
                     ) == 1
                 )
 
-    def _add_single_shift_per_timeslot(self):
+    def _add_single_shift_per_timeslot(self) -> None:
         for nurse in self.schedule.available_nurses:
             for timeslot in self.schedule.month.timeslots:
                 self.Add(
@@ -81,7 +83,7 @@ class ScheduleModel(cp_model.CpModel):
                 )
 
     @TimerLog(logger_name='scheduler.solver')
-    def _add_number_of_shifts_per_nurse(self):
+    def _add_number_of_shifts_per_nurse(self) -> None:
         nurses_with_not_enough_cycle_timeslots = self.schedule.nurses_with_not_enough_cycle_timeslots
         for nurse in self.schedule.available_nurses:
             if nurse not in nurses_with_not_enough_cycle_timeslots:
@@ -95,7 +97,7 @@ class ScheduleModel(cp_model.CpModel):
                 self.Add(sum(planned_shifts) == shifts_to_work)
 
     @TimerLog(logger_name='scheduler.solver')
-    def _add_min_positions(self):
+    def _add_min_positions(self) -> None:
         def add_min_nurse_position(nurse, sector, min_shifts):
             planned_shifts = []
             for timeslot in self.schedule.month.timeslots:
@@ -111,7 +113,7 @@ class ScheduleModel(cp_model.CpModel):
                     add_min_nurse_position(nurse, sector, min_shifts)
 
     @TimerLog(logger_name='scheduler.solver')
-    def _add_max_positions(self):
+    def _add_max_positions(self) -> None:
         def add_max_nurse_position(nurse, sector, max_shifts):
             planned_shifts = []
             for timeslot in self.schedule.month.timeslots:
@@ -126,7 +128,7 @@ class ScheduleModel(cp_model.CpModel):
                 if max_shifts is not None:
                     add_max_nurse_position(nurse, sector, max_shifts)
 
-    def _maximize_positions_per_nurse(self):
+    def _maximize_positions_per_nurse(self) -> None:
         for nurse in self.schedule.available_nurses:
             nurse_positions = []
             for timeslot in self.schedule.month.timeslots:
@@ -136,7 +138,7 @@ class ScheduleModel(cp_model.CpModel):
             self.Maximize(sum([16 - nurse_positions.count(el) for el in list(set(nurse_positions))]))
 
     @TimerLog(logger_name='scheduler.solver')
-    def _add_constraints(self):
+    def _add_constraints(self) -> None:
         self._add_fixed_assignment()
         self._add_no_double_assignment()
         self._add_single_shift_per_timeslot()
@@ -147,7 +149,7 @@ class ScheduleModel(cp_model.CpModel):
 
 
 class SolutionCollector(cp_model.CpSolverSolutionCallback):
-    def __init__(self, variables, schedule):
+    def __init__(self, variables: dict[tuple[int, int, int], cp_model.IntVar], schedule: models.Schedule):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.variables = variables
         self.schedule = schedule
@@ -157,7 +159,7 @@ class SolutionCollector(cp_model.CpSolverSolutionCallback):
         self.solution = {}
 
     @TimerLog(logger_name='scheduler.solver')
-    def on_solution_callback(self):
+    def on_solution_callback(self) -> None:
         for t in self.timeslots:
             for n in self.nurses:
                 for s in self.shifts[str(t)]:
@@ -171,7 +173,7 @@ class SolutionCollector(cp_model.CpSolverSolutionCallback):
                             self.solution[n] = [s]
 
     @cached_property
-    def solution_by_timeslot(self):
+    def solution_by_timeslot(self) -> dict[models.TimeSlot, list[models.Shift]]:
         solution_by_timeslot = {ts: [] for ts in self.timeslots}
         for nurse in self.solution:
             for shift in self.solution[nurse]:
@@ -181,24 +183,24 @@ class SolutionCollector(cp_model.CpSolverSolutionCallback):
 
 class NurseCycleDistributionModel(cp_model.CpModel):
 
-    def __init__(self, schedule):
+    def __init__(self, schedule: models.Schedule):
         super().__init__()
         self.schedule = schedule
         self.variables = self._create_variables()
         self._add_constraints()
 
-    def _create_variables(self):
+    def _create_variables(self) -> dict[tuple[int, int], cp_model.IntVar]:
         variables = {}
         for nurse in self.schedule.available_nurses:
             for cycle in [1, 2, 3, 4]:
                 variables[(nurse.id, cycle)] = self.NewBoolVar(f"{nurse} works on cycle {cycle}")
         return variables
 
-    def _add_each_nurse_is_assigned_to_one_cycle(self):
+    def _add_each_nurse_is_assigned_to_one_cycle(self) -> None:
         for nurse in self.schedule.available_nurses:
             self.Add(sum([self.variables[nurse.id, cycle] for cycle in [1, 2, 3, 4]]) == 1)
 
-    def _add_distribute_nurses_evenly(self):
+    def _add_distribute_nurses_evenly(self) -> None:
         min_nurses_per_cycle = len(self.schedule.available_nurses) // 4
         for cycle in [1, 2, 3, 4]:
             self.Add(sum([self.variables[nurse.id, cycle] for nurse in
@@ -206,7 +208,7 @@ class NurseCycleDistributionModel(cp_model.CpModel):
             # self.Add(sum([self.variables[nurse.id, cycle] for nurse in
             #               self.schedule.available_nurses]) <= min_nurses_per_cycle + 1)
 
-    def _add_distribute_nurses_evenly_by_skills(self):
+    def _add_distribute_nurses_evenly_by_skills(self) -> None:
         for sector in self.schedule.essential_sectors:
             for cycle in [1, 2, 3, 4]:
                 min_nurses_per_cycle = len(self.schedule.available_nurses_per_sector[sector.short_name]) // 4
@@ -218,14 +220,14 @@ class NurseCycleDistributionModel(cp_model.CpModel):
                 self.Add(sum([self.variables[n.id, cycle] for n in
                               self.schedule.nurses_with_7_positions]) >= min_nurses_per_cycle)
 
-    def _add_constraints(self):
+    def _add_constraints(self) -> None:
         self._add_each_nurse_is_assigned_to_one_cycle()
         self._add_distribute_nurses_evenly()
         self._add_distribute_nurses_evenly_by_skills()
 
 
 class NurseDistributionSolutionCollector(cp_model.CpSolverSolutionCallback):
-    def __init__(self, variables, schedule):
+    def __init__(self, variables: dict[tuple[int, int], cp_model.IntVar], schedule: models.Schedule):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.variables = variables
         self.schedule = schedule
