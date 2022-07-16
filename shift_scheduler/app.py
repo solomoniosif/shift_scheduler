8f6d02f06d8da68042807c6f76ab91db08cd8d38
iosif <solomoniosif@gmail.com>
2022-07-16 04:21:18 +0300

    added comments to main function and added feature to get year and moth from user input

--- shift_scheduler/app.py
+++ shift_scheduler/app.py
@@ -16,27 +16,41 @@ app_logger = logging.getLogger('scheduler.app')
 
 
 @TimerLog(logger_name='scheduler.app', text="Schedule generation")
-def main():
-    year, mnth = 2022, 5
-    app_logger.info('Working on a schedule solution for month %s %s', Month.MONTH_NAMES[mnth - 1], year)
+def main() -> None:
+    # Get year and month from user input
+    year = int(input("Enter the year: "))
+    while year not in range(2021, 2051):
+        year = int(input("Entered year must be between 2021 and 2050. \nPlease provide a year in this range: "))
+    month = int(input("Enter the month (1 to 12): "))
+    while month not in range(1, 13):
+        month = int(input("Entered month must be between 1 and 12. \nPlease provide a month in this range: "))
 
-    ss_manager = ScheduleSSManager(year, mnth)
+    # Log to console the start of schedule generation
+    app_logger.info('Working on a schedule solution for month %s %s', Month.MONTH_NAMES[month - 1], year)
+
+    # Initialize models for given month
+    ss_manager = ScheduleSSManager(year, month)
     this_month_ss = ss_manager.get_or_create_new_ss()
     schedule = Schedule(ss_manager)
+
+    # Create the model
     model = ScheduleModel(schedule)
 
+    # Initialize CP-SAT Solver and the solution callback instance
     solver = cp_model.CpSolver()
     solution_collector = SolutionCollector(model.variables, schedule)
 
+    # Search for a feasible solution and save it to a variable
     status = solver.Solve(model, solution_collector)
 
     app_logger.debug("Solution status: %s", solver.StatusName())
 
     response_stats = solver.ResponseStats()
-
     solution = solution_collector.solution
 
-    ss_manager.insert_shifts(schedule.create_schedule_matrix(solution))
+    # Insert solution into target spreadsheet
+    if solver.StatusName() in ['OPTIMAL', 'FEASIBLE']:
+        ss_manager.insert_shifts(schedule.create_schedule_matrix(solution))
 
 
 if __name__ == '__main__':
